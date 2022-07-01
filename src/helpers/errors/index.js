a9a2881db6e2816b218fc97638786211639ffa84
enzo-rs <enzorojoall@gmail.com>
2022-07-01 19:54:22 +0200

    [ADD] - index file

---
+++ src/helpers/errors/index.js
@@ -0,0 +1,13 @@
+const BadRequestError = require('./400_badRequest');
+const UnauthorizedError = require('./401_unauthorized');
+const ForbiddenError = require('./403_forbidden');
+const NotFoundError = require('./404_notFound');
+const InternalServerError = require('./500_internalServerError');
+
+export {
+	BadRequestError,
+	UnauthorizedError,
+	ForbiddenError,
+	NotFoundError,
+	InternalServerError,
+};
